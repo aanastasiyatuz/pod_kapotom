2ba156d358bd6a766c58b0c450fe8edf06d85da7
Arafat <arafatamrullaev021@gmail.com>
2022-08-04 12:17:08 +0600

    models_shop

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    desc = models.TextField()
+    image = models.ImageField(upload_to='products')
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='comments', on_delete=models.CASCADE)
+    body = models.TextField()
+
+class Like(models.Model):
+    user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='likes', on_delete=models.CASCADE)
