53c2d6d1166a0ff19e330cb1b5e322b61a2aed5b
Edilhan <edylhan@gmail.com>
2022-08-04 17:06:17 +0600

    views-register

--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+User = get_user_model()
+
+class RegisterAPIView(APIView):
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response('Account created', 201)
