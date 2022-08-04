2c59f5c3ad79fd96af04de5fd83fe806e953619a
Argen <turdalievargen@gmail.com>
2022-08-04 12:41:37 +0600

    ser

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,3 +1,21 @@
-from rest_framawork import serializers
+from rest_framework import serializers
 
-from .models import Product, Comment, 
+from .models import Product, Comment, Like
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        exclude = ['user']
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context.get('request').user
+        return super().create(validated_data)
+        
+        
