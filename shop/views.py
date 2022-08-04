c7dd0d7498db8feaa574cf18d7637215fd808bc2
Kalys2505 <zarypbekov.kalys@gmail.com>
2022-08-04 13:43:23 +0600

    cud_comment

--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from .models import *
 from rest_framework.response import Response
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from .models import Comment
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import CommentSerializer
+
+class CommentViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin, mixins.UpdateModelMixin, GenericViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
+    
+
 @api_view(["GET"])
 def toggle_like(request, p_id):
     user = request.user
@@ -12,3 +29,4 @@ def toggle_like(request, p_id):
     else:
         Like.objects.filter(user=user, product=product)
     return Response("Like toggled", 200)
+
