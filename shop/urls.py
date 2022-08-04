92a83ce4a2fed9d1670066bf924fdea30318b971
Ertayx <ertaiesen05@gmail.com>
2022-08-04 16:36:41 +0600

    all

--- shop/urls.py
+++ shop/urls.py
@@ -1,16 +1,16 @@
-from rest_framework.routers import DefaultRouter
-from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from django.urls import path, include
 
-from .views import CommentViewSet, ProductViewSet, toggle_like
+# from .views import CommentViewSet, ProductViewSet, toggle_like
 
-router = DefaultRouter()
-router.register('products', ProductViewSet)
-router.register('comments', CommentViewSet)
+# router = DefaultRouter()
+# router.register('products', ProductViewSet)
+# router.register('comments', CommentViewSet)
 
 
 
-urlpatterns = [
-    path('', include(router.urls)),
-    path('toggle_like/<int:p_id>/',toggle_like),
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('toggle_like/<int:p_id>/',toggle_like),
 
-]
+# ]
