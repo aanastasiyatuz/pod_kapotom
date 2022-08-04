4ba3a0be173e71610381ede610713fd0c812f375
Beknaz <choybekov.beknaz@gmail.com>
2022-08-04 12:54:26 +0600

    toggle_like

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from .models import *
+from rest_framework.response import Response
+@api_view(["GET"])
+def toggle_like(request, p_id):
+    user = request.user
+    product = get_object_or_404(Product, id=p_id)
 
-# Create your views here.
+    if Like.objects.filter(user=user, product=product).exists():
+        Like.objects.filter(user=user, product=product).delete()
+    else:
+        Like.objects.filter(user=user, product=product)
+    return Response("Like toggled", 200)
